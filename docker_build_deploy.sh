57e0b25a2bf0cc0de313aa8da0ea7d834a692273
Victor Alexandruk <77550665+victoralexandruk@users.noreply.github.com>
2021-01-18 22:55:39 -0300

    Update docker_build_deploy.sh

--- docker_build_deploy.sh
+++ docker_build_deploy.sh
@@ -2,9 +2,9 @@
 
 echo "Docker build and deploy"
 
-DOCKERFILE_PATH = $1
-IMAGE_NAME = $2
-CONTAINER_NAME = $3
+DOCKERFILE_PATH=$1
+IMAGE_NAME=$2
+CONTAINER_NAME=$3
 
 docker build -f $DOCKERFILE_PATH $(GITHUB_URL_WITH_TOKEN) -t $IMAGE_NAME
 docker stop $CONTAINER_NAME || true && docker rm $CONTAINER_NAME || true
