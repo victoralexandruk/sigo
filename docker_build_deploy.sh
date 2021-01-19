3293300403d7953b68c766612dc10bb023c637aa
Victor Alexandruk <77550665+victoralexandruk@users.noreply.github.com>
2021-01-18 22:34:40 -0300

    Create docker_build_deploy.sh

---
+++ docker_build_deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Docker build and deploy"
+
+DOCKERFILE_PATH = $1
+IMAGE_NAME = $2
+CONTAINER_NAME = $3
+
+docker build -f $DOCKERFILE_PATH $(GITHUB_URL_WITH_TOKEN) -t $IMAGE_NAME
+docker stop $CONTAINER_NAME || true && docker rm $CONTAINER_NAME || true
+docker run --name $CONTAINER_NAME -d -p 52001:443 $IMAGE_NAME
