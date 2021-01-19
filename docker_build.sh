0115bd80cd89acd792df3f4e8b73752ccac00820
Victor Garcia Alexandruk <v.alexandruk@gmail.com>
2021-01-19 00:54:47 -0300

    Ajuste proxy

--- docker_build.sh
+++ docker_build.sh
@@ -4,5 +4,5 @@ echo "Docker build"
 
 GITHUB_URL_WITH_TOKEN=$1
 
-docker build -f SIGO.Normas/Dockerfile $GITHUB_URL_WITH_TOKEN -t sigo/normas:dev
-docker build -f SIGO.WebSite/Dockerfile $GITHUB_URL_WITH_TOKEN -t sigo/website:dev
+#docker build -f SIGO.Normas/Dockerfile $GITHUB_URL_WITH_TOKEN -t sigo/normas:dev
+#docker build -f SIGO.WebSite/Dockerfile $GITHUB_URL_WITH_TOKEN -t sigo/website:dev
