a4a37f5bf9d8133fa8ac012af0fb74df2921656d
Victor Garcia Alexandruk <v.alexandruk@gmail.com>
2021-02-01 16:22:56 -0300

    Ajuste modal

--- SIGO.WebSite/wwwroot/components/ModalAcaoPlanejada.vue
+++ SIGO.WebSite/wwwroot/components/ModalAcaoPlanejada.vue
@@ -1,10 +1,10 @@
 <template lang="html">
   <div class="modal fade" id="modalAcaoPlanejada" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-lg" role="document">
       <div class="modal-content">
         <form v-if="acaoPlanejada" @submit.prevent="saveAcaoPlanejada()">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Ação Planejada</h5>
+            <h5 class="modal-title">Ação Planejada</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
@@ -22,7 +22,7 @@
             </div>
             <div class="form-group">
               <label>Descrição</label>
-              <textarea class="form-control" placeholder="Descrição..." v-model="acaoPlanejada.descricao"></textarea>
+              <textarea class="form-control" rows="5" placeholder="Descrição..." v-model="acaoPlanejada.descricao"></textarea>
             </div>
           </div>
           <div class="modal-footer">
